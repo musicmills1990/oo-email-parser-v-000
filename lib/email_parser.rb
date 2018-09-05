bf95339488e01af0bdc80694584c4b7f82530c77
Marc Mills <mdmills1990@gmail.com>
2018-09-05 14:38:34 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,14 +12,6 @@ end
 
 
 def self.parse(emails)
-  email_array = emails.split("")
-  if email_array.include?(",")
-    array_1 = email_array.split(" , ")
-    return array_1.uniq
-  else
-    array_2 = email_array.split(" ")
-    return array_2.uniq
-  end
 end
 
 

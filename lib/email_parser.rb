1f56c189e224b47a952d645a6909533768b0ab3f
Marc Mills <mdmills1990@gmail.com>
2018-09-05 15:04:53 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,10 +11,16 @@ def initialize(emails)
 end
 
 
-def self.parse
-
-  new_array = emails.split(/\W\w/)
-  new_array.uniq
-end
-
+def self.parse(email_string)
+  #if the string contains a comma, then split the string by each comma and save it to a new array
+  #then after that, join it back together by the space
+  if email_string.include?(", ")
+    email_array = email_string.split(", ")
+    email_string_no_comma = email_array.join(" ")
+    final_email_array = email_string_no_comma.split(" ")
+    final_email_array.uniq
+  else
+    final_email_array_2 = email_string.split(" ")
+    final_email_array_2.uniq
+    end
 end
